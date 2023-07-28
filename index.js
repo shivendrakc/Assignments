7387771fa87ecb84b6c8deb0e80a79161efe05ab
Shivendra <67376433+shivendrakc@users.noreply.github.com>
2023-07-29 00:10:18 +1000

    Added the transition in pop up

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const close_btn = document.querySelector(".close");
 
 btn.addEventListener("click", ()=>{
     card.classList.add("active");
+    btn.style.zIndex = "0";
+    btn.style.opacity = "0";
 })
 close_btn.addEventListener("click", ()=>{
     card.classList.remove("active");
+    btn.style.zIndex = "1";
+    btn.style.opacity = "1";
 })
