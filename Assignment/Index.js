a9bab6e17e276e3619022876f59c299d25ad5d9d
Shivendra <67376433+shivendrakc@users.noreply.github.com>
2023-09-05 09:28:22 +1000

    Made all the final changes to meet the rubric / This completes the project as per rubric / although we have not finished up with designning due to deadline this is the final draft

--- Assignment/Index.js
+++ Assignment/Index.js
@@ -9,3 +9,29 @@ loginButton.addEventListener("click", ()=>{
 registerBtn.addEventListener("click", ()=>{
 	card.style.transform = "rotateY(180deg)";	
 })
+
+// Applying the focus and hover design to meet the rubric although this ca be easily achieved by css in 2 lines of code.
+
+const inputs = document.querySelectorAll("input");
+
+
+inputs.forEach((input)=>{
+	if(input.type !== "submit"){
+		input.addEventListener("focus", ()=>{
+			input.style.backgroundColor = "yellow";
+		})
+		input.addEventListener("blur", ()=>{
+			input.style.backgroundColor = "white";
+		})
+	}
+	if(input.type === "submit"){
+		input.addEventListener('mouseover', ()=>{
+			input.style.backgroundColor = 'lightblue';
+		})
+		input.addEventListener('mouseout', ()=>{
+			input.style.backgroundColor = 'white';
+			input.style.color = 'black';
+		})
+	}
+})
+
